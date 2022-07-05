c256bf119fc6d5f811c0e080406ab70320bc53b4
Justus Springer <justusspringer@gmx.de>
2022-07-05 14:28:40 +0200

    update database tools

--- DatabaseTools.mm
+++ DatabaseTools.mm
@@ -1,8 +1,24 @@
+(*
+Some tools for handling a database of K*-surfaces.
+*)
 
-ImportComplexityOneVarietyList := proc(stmt)
-    local columns, INDEX_S, INDEX_P, INDEX_DIMENSION, INDEX_CLASSGROUP, INDEX_DEGREEMATRIX, INDEX_ANTICANCLASS, INDEX_AMBIENTFAN, INDEX_MAXIMALXCONES, INDEX_GORENSTEININDEX, INDEX_ISGORENSTEIN;
-    local i, clm, result, P, X;
+(*
+Creates a SQLite database for storing K*-surfaces with a given filepath
+*)
+CreateDatabase := proc(filePath :: string, tableName :: string)
+    local db;
+    db := Open(filePath);
+    Execute(db, cat("CREATE TABLE ", tableName, "(r, ns, n, m, s, lss, P, dimension, classGroupRank, classGroup, degreeMatrix, canonicalDivisorClass, ambientFan, maximalXCones, gorensteinIndex, isGorenstein, orderedLss, effectiveCone, movingCone, ampleCone, isFano, variables, monomials, relations, intersectionMatrix, anticanonicalSelfIntersectionFraction, anticanonicalSelfIntersectionFloat, isToric, isIrredundant, isQfactorial, isFactorial, localPicardIndices, picardIndex, isQgorenstein, localGorensteinIndices, localGorensteinQuotients, gorensteinQuotient, case_, orientation, mplus, mminus, betasPlus, betasMinus);"));
+    return db;
+end proc;
 
+(*
+Imports a list of K*-surfaces from a given SQLite statement
+*)
+ImportFromDatabase := proc(stmt)
+    local columns, INDEX_S, INDEX_P, i, clm, result;
+
+    # Find the columns indices for `P` and `s`
     columns := ColumnNames(stmt);
     for i from 0 to nops(columns)-1 do
         clm := columns[i+1];
@@ -10,124 +26,63 @@ ImportComplexityOneVarietyList := proc(stmt)
             INDEX_S := i;
         elif clm = "P" then
             INDEX_P := i;
-        elif clm = "dimension" then
-            INDEX_DIMENSION := i;
-        elif clm = "classGroup" then
-            INDEX_CLASSGROUP := i;
-        elif clm = "degreeMatrix" then
-            INDEX_DEGREEMATRIX := i;
-        elif clm = "anticanClass" then
-            INDEX_ANTICANCLASS := i;
-        elif clm = "ambientFan" then
-            INDEX_AMBIENTFAN := i;
-        elif clm = "maximalXCones" then
-            INDEX_MAXIMALXCONES := i;
-        elif clm = "gorensteinIndex" then
-            INDEX_GORENSTEININDEX := i;
-        elif clm = "isGorenstein" then
-            INDEX_ISGORENSTEIN := i;
         end if;
     end do;
 
-    if not (type(INDEX_S, integer) or type(INDEX_DIMENSION, integer)) then
-        error "Not enough data. You must either provide a column \"s\" (dimension of the acting torus) or a column \"dimension\" (the dimension of the variety = s + 1).";
-    end if;
-
-    if not type(INDEX_S, integer) then
-        error "Not enough data. You must provide a column \"P\" whose entries are the rows of the P-Matrix.";
+    if not type(INDEX_S, integer) or not type(INDEX_P, integer) then
+        error "Not enough data. You must provide the columns `P` and `s`.";
     end if;
 
     result := [];
     while Step(stmt) = RESULT_ROW do
-        P := PMatrix(Fetch(stmt, INDEX_S), Matrix(parse(Fetch(stmt, INDEX_P))), 'skipChecks');
-        # Read in any already avaliable metadata about P, if available
-        if type(INDEX_CLASSGROUP, integer) then
-            setClassGroup(P, parse(Fetch(stmt, INDEX_CLASSGROUP)));
-        end if;
-        if type(INDEX_ANTICANCLASS, integer) then
-            setCanonicalDivisorClass(P, Vector(parse(Fetch(stmt, INDEX_ANTICANCLASS))));
-        end if;
-        if type(INDEX_DEGREEMATRIX, integer) then
-            setDegreeMatrix(P, Matrix(parse(Fetch(stmt, INDEX_DEGREEMATRIX))));
-        end if;
-
-        # If a fan is supplied, make a ComplexityOneVariety.
-        if type(INDEX_AMBIENTFAN, integer) then
-            X := ComplexityOneVariety(P, parse(Fetch(stmt, INDEX_AMBIENTFAN)));
-            if type(INDEX_MAXIMALXCONES, integer) then
-                setMaximalXCones(X, parse(Fetch(stmt, INDEX_MAXIMALXCONES)));
-            end if;
-            if type(INDEX_GORENSTEININDEX, integer) then
-                setGorensteinIndex(X, Fetch(stmt, INDEX_GORENSTEININDEX));
-            end if;
-            if type(INDEX_ISGORENSTEIN, integer) then
-                setIsGorensteinVal(X, evalb(Fetch(stmt, INDEX_ISGORENSTEIN) <> 0));
-            end if;
-            result := [op(result), X];
-        else
-            result := [op(result), P];
-        end if;
+        result := [op(result), ComplexityOneVariety(PMatrix(Fetch(stmt, INDEX_S), Matrix(parse(Fetch(stmt, INDEX_P))), 'skipChecks'))];
     end do;
 
     return result;
 
 end proc;
 
-(* 
-Attempts to find a given Complexity-1 variety `X` in the table `tableName` of a given SQLite connection `db`.
-It returns the list of rowids in the database where the P-Matrix is equivalent to the given one. 
-If the P-Matrix does not occur, it returns the empty list.
-*)
-FindInDatabase := proc(connection, tableName :: string, X :: ComplexityOneVariety)
-    local P, stmtString, stmt, Xs, M, rowids, i, resids;
-    P := X:-P;
-    stmtString := cat("SELECT rowid,* FROM ", tableName, " WHERE ",
-        "m = ", P:-m, " AND ",
-        "s = ", P:-s, " AND ",
-        "classGroup = \"", getClassGroup(X:-P), "\" AND ",
-        "gorensteinIndex = ", getGorensteinIndex(X));
-    # Use the 'lss' as search criterion, only if X is non-toric.
-    if not isToric(P) then
-        stmtString := cat(stmtString, " AND orderedLss = \"", sortColumnsByLss(P):-lss, "\"");
-    end if;
-    
-    stmt := Prepare(connection, stmtString);
-    Xs := ImportComplexityOneVarietyList(stmt);
-    M := FetchAll(stmt):
-    rowids := [seq(M[i,1], i = 1 .. RowDimension(M))];
-    resids := [];
-    for i from 1 to nops(rowids) do
-        if areIsomorphic(X, Xs[i]) then
-            resids := [op(resids), rowids[i]];
-        end if;
-    end do;
-    return resids;
-end proc;
-
 (*
-Imports a single ComplexityOneVariety from a database with a given rowid.
+Imports a single K*-surface from a database with a given rowid.
 To import multiple varieties using an arbitrary SQLite query, use `ImportComplexityOneVarietyList`
 *)
-ImportComplexityOneVariety := proc(db, tableName :: string, rowid :: integer)
-    ImportComplexityOneVarietyList(Prepare(db, cat("SELECT * FROM ", tableName, " WHERE rowid = ", rowid)))[1];
+ImportFromDatabaseID := proc(db, tableName :: string, rowid :: integer)
+    ImportFromDatabase(Prepare(db, cat("SELECT * FROM ", tableName, " WHERE rowid = ", rowid)))[1];
+end proc;
+
+FindInDatabase := proc(db, tableName :: string, X0 :: ComplexityOneVariety)
+    local X, stmt;
+
+    if not X0:-P:-dim = 2 then
+        error "This method only works for K*-surfaces."
+    end if;
+
+    X := normalForm(removeErasableBlocks(X0));
+
+    stmt := Prepare(db, 
+        cat("SELECT rowid FROM ", tableName, " WHERE ",
+        "P = \"", convert(X:-P:-mat, list, nested), "\""));
+
+    return convert(FetchAll(stmt), list);
+
 end proc;
 
 (*
 Given a list of varieties of complexity one `Xs` and a SQLite database connection `db`, this function
-inserts those varieties from `Xs` into the database that are not already present.
+inserts those K*-surfaces from `Xs` into the database that are not already present.
 *)
-ExportComplexityOneVarietyList := proc(connection, tableName :: string, Xs :: list(ComplexityOneVariety))
+ExportToDatabase := proc(connection, tableName :: string, Xs :: list(ComplexityOneVariety))
     local k, X, P, stmt, i, knownCount;
     knownCount := 0;
 
     for k from 1 to nops(Xs) do
-        X := Xs[k];
+        X := normalForm(removeErasableBlocks(Xs[k]));
         
         # Only insert `X` if it is not already present.
         # Can be disabled by the 'noChecks' option
         if 'noChecks' in [_passed] or FindInDatabase(connection, tableName, X) = [] then
             P := X:-P;
-            stmt := Prepare(connection, cat("INSERT INTO ", tableName ," VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"));
+            stmt := Prepare(connection, cat("INSERT INTO ", tableName ," VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"));
             Bind(stmt, 1, P:-r, valuetype = "integer");
             Bind(stmt, 2, convert(P:-ns, string), valuetype = "text");
             Bind(stmt, 3, P:-n, valuetype = "integer");
@@ -167,6 +122,10 @@ ExportComplexityOneVarietyList := proc(connection, tableName :: string, Xs :: li
             Bind(stmt, 37, getGorensteinQuotient(X), valuetype = "integer");
             Bind(stmt, 38, X:-P:-case, valuetype = "text");
             Bind(stmt, 39, X:-P:-orientation, valuetype = "integer");
+            Bind(stmt, 40, X:-P:-mplusInt, valuetype = "integer");
+            Bind(stmt, 41, X:-P:-mminusInt, valuetype = "integer");
+            Bind(stmt, 42, convert(convert(X:-P:-sortedBetasPlus, list), string), valuetype = "text");
+            Bind(stmt, 43, convert(convert(X:-P:-sortedBetasMinus, list), string), valuetype = "text");
 
             Step(stmt);
             Finalize(stmt);
